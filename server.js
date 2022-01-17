14e4d518fc2c8457c2e619cacd3b749248686479
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2022-01-16 19:37:27 -0500

    heroku app error

--- server.js
+++ server.js
@@ -123,10 +123,10 @@ app.use("/api/users", userRoute)
 //app.use("/images", express.static(path.join(__dirname, "/images")))  //makes the images folder public
 // Server static assets if in production
 // //if (process.env.NODE_ENV === 'production') {    
-    app.use(express.static(path.join(__dirname, '../client/build'))) 
+    app.use(express.static(path.join(__dirname, '/client', 'pubic'))) 
 
     app.get('/*', (req, res) => {  
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+        res.sendFile(path.join(__dirname, '/client', 'build', 'index.html'))
     })    
 // //}
 
